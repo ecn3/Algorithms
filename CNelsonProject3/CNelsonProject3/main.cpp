83f9f5507e1cf03496efae1edd7118d30a4a7e80
Christian Nelson <ecn3@students.uwf.edu>
2019-04-11 17:32:32 -0500

    ledger can be called by main

---
+++ CNelsonProject3/CNelsonProject3/main.cpp
@@ -0,0 +1,23 @@
+/*****************************************
+** Starter code for COP3014 Project 03 **
+*****************************************/
+
+/*  Author: Christian Nelon
+*  COP3014 - Project 3
+*  Revision #: TODO
+*/
+
+#include <iostream>
+#include "transaction.hpp"
+#include "ledger.hpp"
+#include "textmenu.hpp"
+
+int main() {
+	Ledger ledger;
+	//TextMenu menu(ledger);
+	TextMenu menu;
+	menu.run();
+
+	return 0;
+}
+
---
+++ CNelsonProject3/CNelsonProject3/textmenu.hpp
@@ -0,0 +1,29 @@
+/*****************************************
+** Starter code for COP3014 Project 03 **
+*****************************************/
+
+#ifndef TEXTMENU_H
+#define TEXTMENU_H
+
+// TODO: include the head file(s) of the class(es) you designed
+
+class TextMenu {
+
+	// TODO: add instance variable(s) of the class(es) you designed
+
+	int selectOptions();
+
+	void addTransaction();
+
+	void printSummary();
+
+	void voidTransaction();
+
+public:
+	// TODO: constructor definition
+
+	void run();
+};
+
+
+#endif //TEXTMENU_H
