bcc68cc9e09e76c4edbc02fe4a240c98fb0e9660
Christian Nelson <ecn3@students.uwf.edu>
2019-04-11 16:56:58 -0500

    added blank methods to ledger

---
+++ CNelsonProject3/CNelsonProject3/ledger.cpp
@@ -0,0 +1,28 @@
+/*****************************************
+** Starter code for COP3014 Project 03 **
+*****************************************/
+
+#include <iostream>
+#include <string>
+#include "ledger.hpp"
+
+using namespace std;
+
+void Ledger::run() {
+
+}
+
+
+void Ledger::addTransaction() {
+
+}
+
+// TODO: constructor implementation here.
+
+void Ledger::printSummary() {
+
+}
+
+void Ledger::voidTransaction() {
+
+}
--- CNelsonProject3/CNelsonProject3/ledger.hpp
+++ CNelsonProject3/CNelsonProject3/ledger.hpp
@@ -1,1 +1,28 @@
-#pragma once
+/*****************************************
+** Starter code for COP3014 Project 03 **
+*****************************************/
+
+#ifndef LEDGER_H
+#define LEDGER_H
+
+// TODO: include the head file(s) of the class(es) you designed
+
+class Ledger {
+
+	// TODO: add instance variable(s) of the class(es) you designed
+
+
+	void addTransaction();
+
+	void printSummary();
+
+	void voidTransaction();
+
+public:
+	// TODO: constructor definition
+
+	void run();
+};
+
+
+#endif //LEDGER_H
