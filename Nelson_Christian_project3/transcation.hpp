d8465e866a1ffa9915320bc4cd877fd7a44aa632
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-04-11 13:07:42 -0500

    created trancation.hpp

---
+++ Nelson_Christian_project3/transcation.hpp
@@ -0,0 +1,25 @@
+/*****************************************
+ ** Starter code for COP3014 Project 03 **
+ *****************************************/
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+
+class Transaction {
+
+
+    void createTransaction();
+
+    void printSummary();
+
+    void updateTransaction();
+
+public:
+
+
+    void run();
+};
+
+
+#endif //TRANSACTION_H
