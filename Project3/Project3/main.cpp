5e03010e050fb8f5973f9d080a1aa03abc25ae0d
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-04-11 14:04:29 -0500

    starts up

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -1,13 +1,22 @@
+/*****************************************
+ ** Starter code for COP3014 Project 03 **
+ *****************************************/
 
- /*  Author: Christian Nelon
+ /*  Author: TODO
   *  COP3014 - Project 3
   *  Revision #: TODO
   */
 
 #include <iostream>
-
+//#include "transaction.hpp"
+//#include "ledger.hpp"
+#include "textmenu.hpp"
 
 int main() {
+	//Ledger ledger;
+	//TextMenu menu(ledger);
+	TextMenu menu;
+	menu.run();
 
 }
 
