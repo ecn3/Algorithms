230aba2f991da0738bb2fff1bda45a159bb3abf5
Christian Nelson <ecn3@students.uwf.edu>
2019-04-11 17:36:10 -0500

    files all working

---
+++ CNelsonProject3/CNelsonProject3/transaction.cpp
@@ -0,0 +1,22 @@
+/*****************************************
+** Starter code for COP3014 Project 03 **
+*****************************************/
+
+#include <iostream>
+#include <string>
+#include "transaction.hpp"
+
+using namespace std;
+
+
+void Transaction::createTransaction() {
+	cout << "ledger.createTransaction" << endl;
+}
+void Transaction::printSummary() {
+	cout << "ledger.printSummary" << endl;
+}
+void Transaction::updateTransaction() {
+	cout << "ledger.updateTransaction" << endl;
+}
+
+
--- CNelsonProject3/CNelsonProject3/transaction.hpp
+++ CNelsonProject3/CNelsonProject3/transaction.hpp
@@ -1,1 +1,25 @@
-#pragma once
+/*****************************************
+** Starter code for COP3014 Project 03 **
+*****************************************/
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+
+class Transaction {
+
+
+	void createTransaction();
+
+	void printSummary();
+
+	void updateTransaction();
+
+public:
+
+
+	//void run();
+};
+
+
+#endif //TRANSACTION_H
