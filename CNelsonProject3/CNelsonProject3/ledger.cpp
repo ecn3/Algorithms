7cd6185610245fdcd1ea065d967d71960f48af8b
Christian Nelson <ecn3@students.uwf.edu>
2019-04-12 15:56:54 -0500

    max transactions at 10

--- CNelsonProject3/CNelsonProject3/ledger.cpp
+++ CNelsonProject3/CNelsonProject3/ledger.cpp
@@ -15,16 +15,23 @@ using namespace std;
 
 void Ledger::addTransaction(double profit, string name) {
 
+	int id = 0;
+	id = randomGenerator(id);
+
+	// only create 10 transactions
+	if (numTransaction == 9) {
+		cout << "Max transactions" << endl;
+	}// makes sure dont add if we have 10 transactions
+	else {
+		Transaction transaction(profit, name, id); // creates new transaction
+		transactions[numTransaction] = transaction; // sets transactions array index of num to transaction
+		numTransaction++; // increases num transaction
+	}
+	//test me
 	cout << "ledger.addTransaction" << endl;
 	cout << "ledger.name: " + name << endl;
 	cout << "ledger.profit: " + std::to_string(profit) << endl;
-	int id = 0;
-	id = randomGenerator(id);
 	cout << "ledger.id: " + std::to_string(id) << endl;
-
-	Transaction transaction(profit, name, id); // creates new transaction
-	transactions[numTransaction] = transaction;
-	numTransaction++;
 }
 
 Ledger::Ledger() {}
