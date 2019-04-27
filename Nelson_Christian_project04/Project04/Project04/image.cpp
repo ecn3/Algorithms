e46123bac09a1d88b5493fdcd7c09b811fbc9d4f
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-04-27 14:14:16 -0500

    created img class

--- Nelson_Christian_project04/Project04/Project04/image.cpp
+++ Nelson_Christian_project04/Project04/Project04/image.cpp
@@ -3,13 +3,54 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 image::image(){}
 
-image::image(std::string infilename, std::string outfilename, std::string version, int numberOfRows, int numberOfColumns, int highValue) {
+image::image(std::string infilename, std::string outfilename, std::string version, int numberOfRows, int numberOfColumns, int highValue, int img[24][7]) {
 	this->infilename = infilename;
 	this->outfilename = outfilename;
 	this->version = version;
 	this->numberOfRows = numberOfRows;
 	this->numberOfColumns = numberOfColumns;
 	this->highValue = highValue;
+	this->img[24][7] = img[24][7]; // img
 }// implement constructor
+
+string image::getInfilename() {
+	return this->infilename;
+}
+string image::getOutfilename() {
+	return this->outfilename;
+}
+string image::getVersion() {
+	return this->version;
+}
+int image::getNumberOfRows() {
+	return this->numberOfRows;
+}
+int image::getNumberOfColumns() {
+	return this->numberOfColumns;
+}
+int image::getHighValue() {
+	return this->highValue;
+}
+
+void image::setInfilename(string infilename) {
+	this->infilename = infilename;
+}
+void image::setOutfilename(string outfilename) {
+	this->outfilename = outfilename;
+}
+void image::setVersion(string version) {
+	this->version = version;
+}
+void image::setNumberOfRows(int numberOfRows) {
+	this->numberOfRows = numberOfRows;
+}
+void image::setNumberOfColumns(int numberOfColumns) {
+	this->numberOfColumns = numberOfColumns;
+}
+void image::setHighValue(int highValue) {
+	this->highValue = highValue;
+}
--- Nelson_Christian_project04/Project04/Project04/image.h
+++ Nelson_Christian_project04/Project04/Project04/image.h
@@ -1,14 +1,29 @@
 #ifndef IMAGE_H
 #define IMAGE_H
-
+#include <string>
 class image
 {
 	int numberOfColumns, numberOfRows, highValue;
-	std::string filename, version;
+	std::string infilename, outfilename, version;
+	int img[24][7];
 public:
 	image(); // defualt constructor
 
-	image(std::string filename, std::string version, int numberOfRows, int numberOfColumns, int highValue);
+	image(std::string infilename, std::string outfilename, std::string version, int numberOfRows, int numberOfColumns, int highValue, int img[24][7]);
+	
+	std::string getInfilename();
+	std::string getOutfilename();
+	std::string getVersion();
+	int getNumberOfRows();
+	int getNumberOfColumns();
+	int getHighValue();
+
+	void setInfilename(std::string infilename);
+	void setOutfilename(std::string outfilename);
+	void setVersion(std::string version);
+	void setNumberOfRows(int numberOfRows);
+	void setNumberOfColumns(int numberOfColumns);
+	void setHighValue(int highValue);
 };
 
 #endif
